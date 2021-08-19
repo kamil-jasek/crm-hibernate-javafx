ae0dbf6d3992be77d893baca7f0dcd9b752f6c1e
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-08-19 13:17:42 +0200

    person form

--- src/main/java/pl/sda/crm/controller/StartViewController.java
+++ src/main/java/pl/sda/crm/controller/StartViewController.java
@@ -1,19 +1,57 @@
 package pl.sda.crm.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import pl.sda.crm.service.PersonCustomerRegistration;
+import pl.sda.crm.service.RegisterPersonForm;
+import pl.sda.crm.util.HibernateUtil;
 
 public class StartViewController {
 
     @FXML
-    private Text typedName;
+    private Text customerId;
 
     @FXML
-    public void onKeyTyped(KeyEvent event) {
-        final var textField = (TextField) event.getSource();
-        final var text = textField.getText();
-        typedName.setText(text);
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField pesel;
+
+    @FXML
+    private Text errorMessage;
+
+    private final PersonCustomerRegistration registration;
+
+    public StartViewController() {
+        this.registration = new PersonCustomerRegistration(HibernateUtil.getSessionFactory());
+    }
+
+    @FXML
+    public void onPersonSave(ActionEvent event) {
+        // 0. Wyczyść poprzedni customerId
+        customerId.setText("");
+
+        // 1. wyciągniecie danych z kontrolek
+        final var firstNameText = firstName.getText();
+        final var lastNameText = lastName.getText();
+        final var peselText = pesel.getText();
+
+        // 2. zbudowanie obiektu RegisterPersonForm
+        final var form = new RegisterPersonForm(firstNameText, lastNameText, peselText);
+
+        // 3. wywołanie registerPerson
+        try {
+            final var registeredCustomerId = registration.registerPerson(form);
+            // 4. wyświetlenie ID zarejestrowanego klienta
+            customerId.setText(registeredCustomerId.getId().toString());
+            errorMessage.setText("");
+        } catch (Exception ex) {
+            errorMessage.setText(ex.getMessage());
+        }
     }
 }
